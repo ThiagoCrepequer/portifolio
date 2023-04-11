464cbb1e4b6acf74aa22470cc09830f29deb15d1
Thiago de Souza Crepequer <45575737+ThiagoCrepequer@users.noreply.github.com>
2023-04-11 17:39:28 +0000

    asd

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
+import Home from "./Pages/Home";
+
 function App() {
   return (
-    <div className="App">
-    </div>
+      <Home />
   );
 }
 
---
+++ src/Components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import styles from './Header.module.scss'
+
+export default function Header() {
+  return (
+    <div styles={styles.header}>
+        <h1>Thiago Crepequer</h1>
+        <p>Engenheiro de Software especialista em desenvolvimento web Full-Stack</p>
+    </div>
+  )
+}
