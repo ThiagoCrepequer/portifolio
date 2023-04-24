9ac64ebbe02707d6a9268eeee6c276dcac122082
Thiago de Souza Crepequer <45575737+ThiagoCrepequer@users.noreply.github.com>
2023-04-24 22:14:46 +0000

    [fix bug] filtro pagina projetos

--- src/Pages/Projetos/TodosProjetos/index.jsx
+++ src/Pages/Projetos/TodosProjetos/index.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import CardProjeto from './CardProjeto'
 import styles from './TodosProjetos.module.scss'
+import NotFound from './NotFound'
 
-export default function TodosProjetos({dados}) {
-    console.log(dados[0].image)
-  return (
-    <div className={styles.container}>
-        {dados.map(dado => (
-            <CardProjeto urls={dado.image} icones={dado.tecnologias} key={dado.title}>{dado.title}</CardProjeto>        
-        ))}
-    </div>
-  )
+export default function TodosProjetos({ dados }) {
+
+    console.log(dados)
+    
+    return (
+        <div className={styles.container}>
+            {
+            dados.length
+            ? dados.map(dado => (
+                 <CardProjeto urls={dado.image} icones={dado.tecnologias} key={dado.title}>{dado.title}</CardProjeto>
+            )) 
+            : <NotFound />
+        }
+        </div>
+    )
 }
