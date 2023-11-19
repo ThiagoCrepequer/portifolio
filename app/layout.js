0b035b58afda60152508417250ba5622beef10db
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-19 13:30:02 -0300

    init v2.0

---
+++ app/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from 'next/font/google'
+import './globals.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export const metadata = {
+    title: 'Thiago | Portifólio',
+    description: 'Thiago Crepequer, desenvolvedor Full-Stack',
+}
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="pt-br">
+            <body className={inter.className}>{children}</body>
+        </html>
+    )
+}
