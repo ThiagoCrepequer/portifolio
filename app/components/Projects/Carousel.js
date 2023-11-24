60a9375e689ff703da949d2ad3cf30887abf9fdc
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-24 20:30:02 -0300

    fix: melhorado logica banner

--- app/components/Projects/Carousel.js
+++ app/components/Projects/Carousel.js
@@ -8,7 +8,8 @@ import './styles.css';
 export default function Carousel() {
     const language = useLanguage();
     const [projects, setProjects] = useState(projetos[language]);
-    const duration = 500;
+    const DELAY_DURATION = 500;
+    const MOVEMENT_SIZE = ((30 / 100) * window.innerWidth);
     let lastScrollTime = 0;
 
     function scrollTo(element, to, duration, callback = () => { }) {
@@ -32,34 +33,23 @@ export default function Carousel() {
         animateScroll();
     }
 
-    const handleResize = (currentProjects = projects) => {
-        const middleIndex = Math.floor(currentProjects.length / 2);
-        const middleProject = document.getElementById(`project_${currentProjects[middleIndex].id}`);
-        const previousProject = document.getElementById(`project_${currentProjects[middleIndex - 1].id}`);
-        // middleProject.setAttribute('style', 'transform: scaleX(1.2);');
-        // previousProject.setAttribute('style', 'transform: scaleX(1);');
-    }
-
     useEffect(() => {
         const carousel = document.getElementById('carousel');
         const cantainerCarousel = document.getElementById('container-carousel');
 
         var horizontalOffset = (cantainerCarousel.scrollWidth - carousel.clientWidth) / 2;
-        scrollTo(carousel, horizontalOffset, duration);
-        handleResize();
+        scrollTo(carousel, horizontalOffset, DELAY_DURATION);
     }, []);
 
     const handleNext = () => {
         if (Date.now() - lastScrollTime < 2000) return;
 
         const carousel = document.getElementById('carousel');
-        scrollTo(carousel, 480, duration, () => {
+        scrollTo(carousel, MOVEMENT_SIZE, DELAY_DURATION, () => {
             const newProjects = [...projects];
             newProjects.push(newProjects.shift());
             setProjects(newProjects);
-            carousel.scrollLeft -= 480;
-            
-            handleResize(newProjects);
+            carousel.scrollLeft -= MOVEMENT_SIZE;
         });
     }
 
@@ -67,23 +57,22 @@ export default function Carousel() {
         if (Date.now() - lastScrollTime < 2000) return;
 
         const carousel = document.getElementById('carousel');
-        scrollTo(carousel, -380, duration, () => {
+        scrollTo(carousel, -MOVEMENT_SIZE, DELAY_DURATION, () => {
             const newProjects = [...projects];
             newProjects.unshift(newProjects.pop());
-            setProjects(newProjects);
-            carousel.scrollLeft += 400;
+            carousel.scrollLeft += MOVEMENT_SIZE;
         });
     }
 
     return (
         <section className="relative">
-            <div className="overflow-hidden" id="carousel">
+            <div className="overflow-auto" id="carousel">
                 <button className="absolute left-0 top-0 h-full" onClick={handleBack}>
                     Anterior
                 </button>
-                <div className="flex flex-nowrap w-full" id="container-carousel">
+                <div className="flex flex-nowrap w-[200vw]" id="container-carousel">
                     {projects.map((projeto) => (
-                        <div key={projeto.id} id={`project_${projeto.id}`} className="teste min-w-[300px] h-[200px] mx-10 border bg-red-700">
+                        <div key={projeto.id} id={`project_${projeto.id}`} className="teste min-w-[25vw] h-[200px] mx-10 border bg-red-700">
                             <h1>{projeto.title}</h1>
                             <h2>{projeto.technologies}</h2>
                             <p>{projeto.description}</p>
