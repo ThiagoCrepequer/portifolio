cd9abd509f8d376f2eda853fdd6a740b411e29b8
Thiago Crepequer <thiago.crepequer@hotmail.com>
2023-11-24 11:24:15 -0300

    feat: melhorado carousel

--- app/components/Projects/Carousel.js
+++ app/components/Projects/Carousel.js
@@ -3,29 +3,76 @@
 import projetos from "@/constants/language/projects.json";
 import useLanguage from "@/hooks/useLanguage";
 import { useEffect, useState } from "react";
+import './styles.css';
 
 export default function Carousel() {
     const language = useLanguage();
     const [projects, setProjects] = useState(projetos[language]);
+    const duration = 500;
+    let lastScrollTime = 0;
+
+    function scrollTo(element, to, duration, callback = () => { }) {
+        const start = element.scrollLeft;
+        const change = to;
+        const increment = 20;
+        let currentTime = 0;
+        lastScrollTime = Date.now();
+
+        function animateScroll() {
+            currentTime += increment;
+            const val = easeInOutQuad(currentTime, start, change, duration);
+            element.scrollLeft = val;
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            } else {
+                callback();
+            }
+        }
+
+        animateScroll();
+    }
+
+    const handleResize = (currentProjects = projects) => {
+        const middleIndex = Math.floor(currentProjects.length / 2);
+        const middleProject = document.getElementById(`project_${currentProjects[middleIndex].id}`);
+        const previousProject = document.getElementById(`project_${currentProjects[middleIndex - 1].id}`);
+        // middleProject.setAttribute('style', 'transform: scaleX(1.2);');
+        // previousProject.setAttribute('style', 'transform: scaleX(1);');
+    }
 
     useEffect(() => {
         const carousel = document.getElementById('carousel');
         const cantainerCarousel = document.getElementById('container-carousel');
 
         var horizontalOffset = (cantainerCarousel.scrollWidth - carousel.clientWidth) / 2;
-        carousel.scrollLeft = horizontalOffset;
+        scrollTo(carousel, horizontalOffset, duration);
+        handleResize();
     }, []);
 
     const handleNext = () => {
-        const newProjects = [...projects];
-        newProjects.push(newProjects.shift());
-        setProjects(newProjects);
+        if (Date.now() - lastScrollTime < 2000) return;
+
+        const carousel = document.getElementById('carousel');
+        scrollTo(carousel, 480, duration, () => {
+            const newProjects = [...projects];
+            newProjects.push(newProjects.shift());
+            setProjects(newProjects);
+            carousel.scrollLeft -= 480;
+            
+            handleResize(newProjects);
+        });
     }
 
     const handleBack = () => {
-        const newProjects = [...projects];
-        newProjects.unshift(newProjects.pop());
-        setProjects(newProjects);
+        if (Date.now() - lastScrollTime < 2000) return;
+
+        const carousel = document.getElementById('carousel');
+        scrollTo(carousel, -380, duration, () => {
+            const newProjects = [...projects];
+            newProjects.unshift(newProjects.pop());
+            setProjects(newProjects);
+            carousel.scrollLeft += 400;
+        });
     }
 
     return (
@@ -35,8 +82,8 @@ export default function Carousel() {
                     Anterior
                 </button>
                 <div className="flex flex-nowrap w-full" id="container-carousel">
-                    {projects.map((projeto, index) => (
-                        <div key={index} className="min-w-[300px] h-[200px] mx-10 border">
+                    {projects.map((projeto) => (
+                        <div key={projeto.id} id={`project_${projeto.id}`} className="teste min-w-[300px] h-[200px] mx-10 border bg-red-700">
                             <h1>{projeto.title}</h1>
                             <h2>{projeto.technologies}</h2>
                             <p>{projeto.description}</p>
@@ -49,4 +96,11 @@ export default function Carousel() {
             </div>
         </section>
     )
+}
+
+function easeInOutQuad(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return (c / 2) * t * t + b;
+    t--;
+    return (-c / 2) * (t * (t - 2) - 1) + b;
 }
